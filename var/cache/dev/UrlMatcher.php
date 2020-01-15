c61a84497cf3918650ea821633ef84e6aac359b3
mnicodev <nicolasmoinot@webstep.fr>
2020-01-15 14:36:36 +0100

    recommit

--- var/cache/dev/UrlMatcher.php
+++ var/cache/dev/UrlMatcher.php
@@ -13,16 +13,15 @@ return [
         '/_profiler/search_bar' => [[['_route' => '_profiler_search_bar', '_controller' => 'web_profiler.controller.profiler::searchBarAction'], null, null, null, false, false, null]],
         '/_profiler/phpinfo' => [[['_route' => '_profiler_phpinfo', '_controller' => 'web_profiler.controller.profiler::phpinfoAction'], null, null, null, false, false, null]],
         '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
+        '/bloc' => [[['_route' => 'bloc', '_controller' => 'App\\Controller\\BlocController::index'], null, null, null, false, false, null]],
         '/admin/page' => [[['_route' => 'admin.page', '_controller' => 'App\\Controller\\PageBuilderController::index'], null, null, null, false, false, null]],
         '/admin/page/builder' => [[['_route' => 'admin.page.create', '_controller' => 'App\\Controller\\PageBuilderController::builder'], null, null, null, false, false, null]],
         '/admin/page/save' => [[['_route' => 'admin.page.save', '_controller' => 'App\\Controller\\PageBuilderController::save'], null, null, null, false, false, null]],
         '/admin/page/create' => [[['_route' => 'admin.popup.page.create', '_controller' => 'App\\Controller\\PageBuilderController::popupCreate'], null, null, null, false, false, null]],
         '/admin/zone/add' => [[['_route' => 'admin.popup.zone.add', '_controller' => 'App\\Controller\\PageBuilderController::popupZoneAdd'], null, null, null, false, false, null]],
-        '/admin/zone/action' => [[['_route' => 'admin.popup.zone.action', '_controller' => 'App\\Controller\\PageBuilderController::popupZoneAction'], null, null, null, false, false, null]],
-        '/admin/bloc/action' => [[['_route' => 'admin.popup.bloc.action', '_controller' => 'App\\Controller\\PageBuilderController::popupBlocAction'], null, null, null, false, false, null]],
-        '/admin/content/action' => [[['_route' => 'admin.popup.content.action', '_controller' => 'App\\Controller\\PageBuilderController::popupContentAction'], null, null, null, false, false, null]],
-        '/admin/content/add' => [[['_route' => 'admin.popup.content.add', '_controller' => 'App\\Controller\\PageBuilderController::popupContentAdd'], null, null, null, false, false, null]],
-        '/' => [[['_route' => 'page.home', '_controller' => 'App\\Controller\\PageController::index'], null, null, null, false, false, null]],
+        '/admin/zone/styles' => [[['_route' => 'admin.popup.styles', '_controller' => 'App\\Controller\\PageBuilderController::popupStyles'], null, null, null, false, false, null]],
+        '/admin/content/add' => [[['_route' => 'admin.popup.content.add', '_controller' => 'App\\Controller\\PageBuilderController::popupAddContent'], null, null, null, false, false, null]],
+        '/' => [[['_route' => 'home', '_controller' => 'App\\Controller\\PageController::index'], null, null, null, false, false, null]],
     ],
     [ // $regexpList
         0 => '{^(?'
@@ -41,7 +40,17 @@ return [
                         .'|(*:159)'
                     .')'
                 .')'
-                .'|/admin/page/builder/([0-9]*)(*:197)'
+                .'|/a(?'
+                    .'|dmin/page/(?'
+                        .'|test/([0-9]*)(*:200)'
+                        .'|builder/([0-9]*)(*:224)'
+                    .')'
+                    .'|ssets/styles([^/\\.]++)\\.css(*:260)'
+                .')'
+                .'|/page/(?'
+                    .'|([^/]++)(*:286)'
+                    .'|load/([^/]++)(*:307)'
+                .')'
             .')/?$}sDu',
     ],
     [ // $dynamicRoutes
@@ -52,8 +61,12 @@ return [
         136 => [[['_route' => '_profiler_exception', '_controller' => 'web_profiler.controller.exception::showAction'], ['token'], null, null, false, false, null]],
         149 => [[['_route' => '_profiler_exception_css', '_controller' => 'web_profiler.controller.exception::cssAction'], ['token'], null, null, false, false, null]],
         159 => [[['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null]],
-        197 => [
-            [['_route' => 'admin.page.edit', '_controller' => 'App\\Controller\\PageBuilderController::edit'], ['slug'], null, null, false, true, null],
+        200 => [[['_route' => 'admin.page.edit2', '_controller' => 'App\\Controller\\PageBuilderController::edit2'], ['slug'], null, null, false, true, null]],
+        224 => [[['_route' => 'admin.page.edit', '_controller' => 'App\\Controller\\PageBuilderController::edit'], ['slug'], null, null, false, true, null]],
+        260 => [[['_route' => 'assets.css', '_controller' => 'App\\Controller\\PageController::css'], ['id'], null, null, false, false, null]],
+        286 => [[['_route' => 'page.view', '_controller' => 'App\\Controller\\PageController::page'], ['id'], null, null, false, true, null]],
+        307 => [
+            [['_route' => 'page.load', '_controller' => 'App\\Controller\\PageController::load'], ['id'], null, null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],
     ],
